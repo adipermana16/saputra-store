34bd28d59a0edea86661699ba7bb28616096232c
adipermana16 <adiemokonkz16@gmail.com>
2023-11-15 13:19:59 +0700

    pengumpulan uts

--- webb/admin.py
+++ webb/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from.models import jual,beli
 # Register your models here.
+admin.site.register(jual)
+admin.site.register(beli)
--- webb/models.py
+++ webb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class jual(models.Model):
+    nama = models.CharField(max_length=200)
+    keterangan = models.TextField()
+    
+    def __str__(self):
+        return f"{self.nama}"
+    
+class beli(models.Model):
+    nama = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"{self.nama}"
